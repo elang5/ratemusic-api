497927440027567e1b1488e6835fe08f2bede9ef
elangreen <green.elan5@gmail.com>
2019-06-07 08:20:14 -0700

    changed Buffer

--- src/spotifyrefresh.js
+++ src/spotifyrefresh.js
@@ -4,7 +4,7 @@ const request = require('request')
 const getAuthToken = (callback) => {
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
+    headers: { 'Authorization': 'Basic ' + (Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
     form: {
       grant_type: 'refresh_token',
       refresh_token: process.env.REFRESH_TOKEN

10533029a97f226b49337cc62a15d888dfbac749
elangreen <green.elan5@gmail.com>
2019-06-06 19:49:41 -0700

    fixed id bug

--- src/albums/albums-router.js
+++ src/albums/albums-router.js
@@ -15,10 +15,11 @@ albumsRouter
   .get((req, res, next) => {
     const authToken = req.app.get('spotifyAuthToken')
     spotifyApi.setAccessToken(authToken)
-    console.log(authToken)
     AlbumsService.getAlbumIds(req.app.get('db'))
       .then(albumIds => {
-        const preppedIds = albumIds.map(alb => alb.album_id)
+        const preppedIds = albumIds
+          .map(alb => alb.album_id)
+          .filter((id, index, self) => self.indexOf(id) === index)
         spotifyApi.getAlbums(preppedIds)
           .then(albums => res.json(albums.body.albums))
           .catch(err => res.json(err))
